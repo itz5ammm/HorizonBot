c713ab8ca85294445b6822f95819de95e05a808d
Glitch (hello-express) <none>
2020-04-23 13:33:37 +0000

    ☄️🍫 Checkpoint ./commands/play.js:5895069/5598

--- commands/play.js
+++ commands/play.js
@@ -1,72 +1,96 @@
-const { Util } = require("discord.js")
-const { YOUTUBE_API_KEY } = require("../config.json")
-const ytdl = require("ytdl-core")
-const YoutubeAPI = require("simple-youtube-api")
-const youtube = new YoutubeAPI(YOUTUBE_API_KEY)
-
+const { Util } = require("discord.js");
+const { YOUTUBE_API_KEY } = require("../config.json");
+const ytdl = require("ytdl-core");
+const YoutubeAPI = require("simple-youtube-api");
+const youtube = new YoutubeAPI(YOUTUBE_API_KEY);
 
 module.exports = {
   name: "play",
   description: "PLAY THE SOFTNESS OF THE SOUND",
   async execute(client, message, args) {
     //FIRST OF ALL WE WILL ADD ERROR MESSAGE AND PERMISSION MESSSAGE
-    if(!args.length) { //IF AUTHOR DIDENT GIVE URL OR NAME
-      return message.channel.send("WRONG SYNTAX : Type `play <URL> or text`")
+    if (!args.length) {
+      //IF AUTHOR DIDENT GIVE URL OR NAME
+      return message.channel.send("WRONG SYNTAX : Type `play <URL> or text`");
     }
-    
+
     const { channel } = message.member.voice;
-    if(!channel) { //IF AUTHOR IS NOT IN VOICE CHANNEL
-      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/")
+    if (!channel) {
+      //IF AUTHOR IS NOT IN VOICE CHANNEL
+      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
     }
-    
+
     //WE WILL ADD PERMS ERROR LATER :(
-    
-    const targetsong = args.join(" ") 
-     const videoPattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
+
+    const targetsong = args.join(" ");
+    const videoPattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
     const playlistPattern = /^.*(youtu.be\/|list=)([^#\&\?]*).*/gi;
-    const urlcheck = videoPattern.test(args[0])
-    
-    if(!videoPattern.test(args[0]) && !playlistPattern.test(args[0])) {
-      return message.channel.send("PLAYLIST CANNOT BE PLAYED")
+    const urlcheck = videoPattern.test(args[0]);
+
+    if (!videoPattern.test(args[0]) && !playlistPattern.test(args[0])) {
+      return message.channel.send("PLAYLIST CANNOT BE PLAYED");
     }
-    
+
     const serverQueue = message.client.queue.get(message.guild.id);
-    
+
     const queueBase = {
-      textChannel : message.channel,
+      textChannel: message.channel,
       channel,
       connection: null,
       songs: [],
       loop: false,
       volume: 100,
       playing: true
-    }
-    
-    let songData = null
-    let song = null
-    
-    if(urlcheck) {
+    };
+
+    let songData = null;
+    let song = null;
+
+    if (urlcheck) {
       try {
-       songData = await ytdl.getInfo(args[0]);
+        songData = await ytdl.getInfo(args[0]);
         song = {
           title: songData.title,
           url: songData.video_url,
           duration: songData.length_seconds
-        }
-        
+        };
       } catch (error) {
-        if(message.include === "copyright") {
-          return message.reply("THERE IS COPYRIGHT CONTENT IN VIDEO -_-")
-          .catch(console.error)
+        if (message.include === "copyright") {
+          return message
+            .reply("THERE IS COPYRIGHT CONTENT IN VIDEO -_-")
+            .catch(console.error);
         } else {
           console.error(error);
         }
-        
       }
     } else {
-      
+      try {
+        const result = await youtube.searchVideos(targetsong, 1)
+        songData = await ytdl.getInfo(result[0].url)
+         song = {
+          title: songData.title,
+          url: songData.video_url,
+          duration: songData.length_seconds
+        };
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    
+    if(!serverQueue) message.client.queue.set(message.guild.id, queueBase)
+    
+    if(!serverQueue) {
+      try {
+        queueBase.connection = await channel.join()
+      //  play(queueBase.songs[0], message) WE DIDENT DEFINED PLAY YET
+      } catch (erorr) {
+        console.error("I am unable to join voice channel")
+        message.client.queue.delete(message.guild.id)
+        await channel.leave();
+        return message.channel.send("I AM UNABLE TO JOIN VOICE CHANNEL").catch(console.error)
+      }
     }
     
     
-    
-  }}
+  }
+};
