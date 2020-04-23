0e995b9abae0cf469f222ce1a671c09b0ad1d3e4
Glitch (hello-express) <none>
2020-04-23 12:34:38 +0000

    🐝🏢 Checkpoint ./config.json:5895069/107 ./server.js:5895069/769

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
 const { readdirSync } = require("fs");
 const { join } = require("path");
-
+const { TOKEN, PREFIX } = require("./config.json")
 
 //CLIENT EVENTS
 client.on("ready", () => {
@@ -14,13 +14,34 @@ client.on("warn", info => console.log(info));
 
 client.on("error", console.error)
 
+//DEFINIING
+client.commands = new discord.Collection()
+client.prefix = PREFIX
+client.queue = new Map();
+
+//WHEN SOMEONE MESSAGE
 client.on("message", message => {
    if (message.author.bot) return;
   if (!message.guild) return;
+  
+  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+    
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+    
+    
+    
+  }
+  
+  
 });
 
 
 
 
 //DONT DO ANYTHING WITH THIS TOKEN lol
-client.login("NjQwMTk3ODA2MzEyOTE0OTYz.XqGITA.andWQ59uXgafpdOYlyz1q3xAgTE")
+client.login(TOKEN)
