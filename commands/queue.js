ad6ffebac9af3b5d1325053077c45fd8f4c5414f
Glitch (hello-express) <none>
2020-04-24 07:06:14 +0000

    🍠🕰 Checkpoint ./commands/queue.js:5895069/1858

---
+++ commands/queue.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "queue",
+  description: "get list of added songs",
+  execute: (client, message, args) => {
+    
+     const { channel } = message.member.voice;
+    if (!channel) {
+      //IF AUTHOR IS NOT IN VOICE CHANNEL
+      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
+    }
+    
+     const serverQueue = message.client.queue.get(message.guild.id)
+
+    if(!serverQueue) {
+      return message.channel.send("There is nothing in the queue")
+    }
+    
+   message.channel.send(`${serverQueue.songs.map((song, index) => index + 1 + '. ' + song.title).join("/n/n")}`, {split: true})
+    
+
+    
+    
+  }
+}
