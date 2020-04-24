3ec0128856cc6eacec1e901b77eb1621e905fa21
Glitch (hello-express) <none>
2020-04-24 06:54:20 +0000

    🌯😽 Checkpoint ./commands/stop.js:5895069/817 ./commands/queue.js:5895069/15

---
+++ commands/stop.js
@@ -0,0 +1,27 @@
+const discord = require("discord.js")
+
+
+module.exports = {
+  name: "stop",
+  description: "Stop the music",
+  execute(client, message, args)  {
+ 
+     const { channel } = message.member.voice;
+    if (!channel) {
+      //IF AUTHOR IS NOT IN VOICE CHANNEL
+      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
+    }
+    
+    const serverQueue = message.client.queue.get(message.guild.id)
+
+    if(!serverQueue) {
+      return message.channel.send("There is nothing playing that i could stop")
+    }
+    
+    serverQueue.songs = []
+    serverQueue.connection.dispatcher.end();
+    
+    serverQueue.textChannel.send("**Stoped the song form playing music**")
+  
+    
+}}
