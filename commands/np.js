e54f9e74a71bf786f81b2bd98b6955d3dc96ae09
Glitch (hello-express) <none>
2020-04-24 08:51:15 +0000

    🤗⏰ Checkpoint ./commands/np.js:5895069/653

---
+++ commands/np.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "np",
+  description: "send the name of on going song",
+  execute (client, message, args) {
+    
+      const { channel } = message.member.voice;
+    if (!channel) {
+      //IF AUTHOR IS NOT IN VOICE CHANNEL
+      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
+    }
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+
+    if (!serverQueue) {
+      return message.channel.send("Bot is not playing anything");
+    }
+    
+    message.channel.send(serverQueue.songs[0].title)
+
+    
+    
+    
+  }
+}
