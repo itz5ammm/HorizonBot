8d24f87b6db75ac8cb25af269046e1c15f94ee6c
Glitch (hello-express) <none>
2020-04-24 07:52:15 +0000

    💖🍇 Checkpoint ./commands/lyrics.js:5895069/346 ./package.json:5895069/1695

---
+++ commands/lyrics.js
@@ -0,0 +1,12 @@
+const Genius = new (require("genius-lyrics"))("ApavK7sxIw4WfaTNVe1g9Hc8civ8WqGW0NWx_akrti6Bcg3Nc7ILibv9LoVDoT0-");
+
+module.exports = {
+  name: "lyrics", 
+  description: "Get lyrics of Song",
+  execute (client, message, args) {
+    
+  
+    
+    
+  }
+}
