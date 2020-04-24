0b9bb8dd06352999f7cd892e1cf0a0853fadf494
Glitch (hello-express) <none>
2020-04-24 08:03:20 +0000

    🐗🏎 Checkpoint ./commands/lyrics.js:5895069/2581

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -5,7 +5,35 @@ module.exports = {
   description: "Get lyrics of Song",
   execute (client, message, args) {
     
-  
+     const { channel } = message.member.voice;
+    if (!channel) {
+      //IF AUTHOR IS NOT IN VOICE CHANNEL
+      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
+    }
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+
+    if (!serverQueue) {
+      return message.channel.send("There is nothing that bot is playing");
+    }
+    
+    
+    //NOw we gonna see on playing song
+  Genius.tracks.search(serverQueue.songs[0].title)
+.then(results => {
+    const song = results[0];
+    song.lyrics()
+    .then(lyrics => {
+      
+      
+      if(lyrics.length > 2000) {
+          message.channel.send(lyrics.slice(0, 2000))
+      }
+      
+      
+        message.channel.send(lyrics)
+    })
+}).catch(err => message.channel.send("Unable to find lyrics"));
     
     
   }
