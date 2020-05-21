5e597e844ec360eef5ce77dc8fd55a148af4e3a9
Glitch (hello-express) <none>
2020-05-21 19:37:29 +0000

    🐸🍉 Checkpoint ./commands/skip.js:5895069/800

--- commands/skip.js
+++ commands/skip.js
@@ -31,12 +31,37 @@ const vote = message.client.vote.get(message.guild.id)
     }
     
     const vcvote = Math.floor(message.guild.me.voice.channel.members.size / 2)
+    const okie = Math.floor(message.guild.me.voice.channel.members.size / 2 - 1)
     console.log(message.guild.me.voice.channel.members.size)
      if(message.member.hasPermission("ADMINISTRATOR")) {
+       if(vote.vote > okie) {
+         serverQueue.connection.dispatcher.end();
+    embed.setDescription("VOTE - SKIP | Skipping The Song")
+    embed.setThumbnail(client.user.displayAvatarURL())
+    return message.channel.send(embed);
+       }
+       
+       if(vote.voters.includes(message.author.id)) {
+         return message.channel.send("You already voted for this song")
+       }
+       
+       if(vcvote === 2) {
+          serverQueue.connection.dispatcher.end();
+    embed.setDescription("✔ | Skipping The Song")
+    embed.setThumbnail(client.user.displayAvatarURL())
+    return message.channel.send(embed);
+       }
+       
+       
+       
 vote.vote++
        vote.voters.push(message.author.id)
        return message.channel.send(`You Voted for the Song to Skip, btw we currently need ${Math.floor(vcvote - vote.vote)} votes`)
-    }
+    
+     
+     
+     
+     }
 
     serverQueue.connection.dispatcher.end();
     embed.setDescription("✔ | Skipping The Song")
