034439c84ba55109a5754c36b2edee8fcdaa0669
Glitch (hello-express) <none>
2020-05-21 16:19:20 +0000

    🏢🏀 Checkpoint ./commands/help.js:5895069/581 ./commands/loop.js:5895069/441

--- commands/help.js
+++ commands/help.js
@@ -8,21 +8,24 @@ module.exports = {
     
     
 let embed = new MessageEmbed()
-.setAuthor("HELP SECTION", client.user.avatarURL())
-.setColor("RANDOM");
-    
+.setAuthor("HELP SECTION")
+.setColor("RANDOM")
+.setDescription(`These are the command ${client.user.username} Bot, INVITE ME - LINK`)
 let command = readdirSync("./commands")    
 
-console.log(command)
+let i;
+    for(i = 0; i < command.length; i++) {
+      console.log(command[i])
+      
+      const cmd = client.commands.get(command[i].replace(".js", ""))
+      embed.addField(`**${cmd.name}**`, cmd.description, true)
+      
+    }
+    
+    message.channel.send(embed)
     
     
-    
-    
-    
-    
-    
-    
-    
+
     
     
     
--- commands/loop.js
+++ commands/loop.js
@@ -1,3 +1,8 @@
+const { MessageEmbed } = require("discord.js")
+
+let embed = new MessageEmbed()
+.setColor("RANDOM");
+
 module.exports = {
   name: "loop",
   description: "LOOP THE QUEUE",
@@ -6,20 +11,22 @@ module.exports = {
     const { channel } = message.member.voice;
     if (!channel) {
       //IF AUTHOR IS NOT IN VOICE CHANNEL
-      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
+      embed.setAuthor("YOU NEED TO BE IN VOICE CHANNEL :/")
+      return message.channel.send(embed);
     }
 
     const serverQueue = message.client.queue.get(message.guild.id);
 
     if (!serverQueue) {
-      return message.channel.send("There is nothing playing that i could loop");
+      embed.setAuthor("There is nothing playing that i could loop")
+      return message.channel.send(embed);
     }
     
     //OOOOF
     serverQueue.loop = !serverQueue.loop
     
     
-    
+    embed.setDescription()
     message.channel.send(`Loop is now **${serverQueue.loop ? "Enabled" : "Disabled"}**`)
     
     
