28c55b29d02a8eb764046c1d8f06ea84a449a7f8
Glitch (hello-express) <none>
2020-05-22 08:07:17 +0000

    🏨🐲 Checkpoint ./commands/shuffle.js:5895069/2199

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,31 +1,43 @@
-const { MessageEmbed } = require("discord.js")
-const { COLOR } = require("../config.json")
+const { MessageEmbed } = require("discord.js");
+const { COLOR } = require("../config.json");
 
 module.exports = {
   name: "shuffle",
   description: "Shuffle your queue",
-  execute (client, message, args) {
-    
-    let embed = new MessageEmbed()
-    .setColor(COLOR)
-    
-   const { channel } = message.member.voice;
+  execute(client, message, args) {
+    let embed = new MessageEmbed().setColor(COLOR);
+
+    const { channel } = message.member.voice;
     if (!channel) {
       //IF AUTHOR IS NOT IN VOICE CHANNEL
-      embed.setAuthor("YOU NEED TO BE IN VOICE CHANNEL :/")
+      embed.setAuthor("YOU NEED TO BE IN VOICE CHANNEL :/");
       return message.channel.send(embed);
     }
 
     const serverQueue = message.client.queue.get(message.guild.id);
 
     if (!serverQueue) {
-      embed.setAuthor("There is nothing playing that i could loop")
+      embed.setAuthor("There is nothing playing that i could loop");
       return message.channel.send(embed);
     }
+
+    let title = [];
+
+    serverQueue.slice(0, 10).forEach(obj => {
+      title.push(obj.title)
+      
+    });
     
-    shuffleQueue()
     
+    var num = 10;
     
-    
+    if(title.length < 10) num = title.length;
+    embed.setTitle("NEW QUEUE")
+    embed.setDescription(`This is a new shuffled queue`)
+  var i;
+    for(i = 0; i < num; i++) {
+      embed.addField(`SONG ${i + 1}`, title[i])
+    }
+    message.channel.send(embed)
   }
-}
+};
