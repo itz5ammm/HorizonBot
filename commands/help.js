285e0965ce8d904692e7d9ad19f161bd40da1a21
Glitch (music-tuto) <none>
2020-08-10 15:01:52 +0000

    🗼🍯 Checkpoint ./commands/help.js:20970236/1707 ./config.json:20970236/857

--- commands/help.js
+++ commands/help.js
@@ -1,34 +1,27 @@
-const { MessageEmbed } = require("discord.js")
-const { readdirSync } = require("fs")
+const { MessageEmbed } = require("discord.js");
+const { readdirSync } = require("fs");
 const { COLOR } = require("../config.json");
 module.exports = {
   name: "help",
   description: "Get all commands name and description",
-  execute (client, message, args) {
-    
-    
-let embed = new MessageEmbed()
-.setAuthor("HELP SECTION", client.user.displayAvatarURL())
-.setThumbnail(client.user.displayAvatarURL())
-.setColor(COLOR)
-.setDescription(`These are the command ${client.user.username} Bot, INVITE ME - LINK`)
-let command = readdirSync("./commands")    
+  execute(client, message, args) {
+    let embed = new MessageEmbed()
+      .setAuthor("HELP SECTION", client.user.displayAvatarURL())
+      .setThumbnail(client.user.displayAvatarURL())
+      .setColor(COLOR)
+      .setDescription(
+        `These are the command ${client.user.username} Bot, INVITE ME - LINK`
+      );
+    let command = readdirSync("./commands");
 
-let i;
-    for(i = 0; i < command.length; i++) {
-      console.log(command[i])
-      
-      const cmd = client.commands.get(command[i].replace(".js", ""))
-      embed.addField(`**${cmd.name}**`, cmd.description, true)
-      
+    let i;
+    for (i = 0; i < command.length; i++) {
+      console.log(command[i]);
+
+      const cmd = client.commands.get(command[i].replace(".js", ""));
+      embed.addField(`**${cmd.name}**`, cmd.description, true);
     }
-    
-    message.channel.send(embed)
-    
-    
 
-    
-    
-    
+    message.channel.send(embed);
   }
-}
+};
