c68aa297c7fc50e4973b2017b69f8f03764362ac
Glitch (music-tuto) <none>
2020-08-11 10:27:02 +0000

    🍨💨 Checkpoint ./server.js:21005878/3704

--- server.js
+++ server.js
@@ -1,63 +1,71 @@
-const discord = require("discord.js")
-const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const discord = require("discord.js");
+const client = new discord.Client({
+  disableEveryone: true,
+  disabledEvents: ["TYPING_START"]
+});
 const { readdirSync } = require("fs");
 const { join } = require("path");
-const { TOKEN, PREFIX } = require("./config.json")
+const { TOKEN, PREFIX } = require("./config.json");
 
 //CLIENT EVENTS
 client.on("ready", () => {
-  console.log('Ready to play song | Bot created by CTK WARRIOR')
-  client.user.setActivity("x!help | Musix")
-})
+  console.log("Ready to play song | Bot created by CTK WARRIOR");
+  client.user.setActivity("!help | FaNG");
+});
 
 client.on("warn", info => console.log(info));
 
-client.on("error", console.error)
+client.on("error", console.error);
 
 //DEFINIING
-client.commands = new discord.Collection()
-client.prefix = PREFIX
+client.commands = new discord.Collection();
+client.prefix = PREFIX;
 client.queue = new Map();
 client.vote = new Map();
 
 //LETS LOAD ALL FILES
-const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"))
+const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file =>
+  file.endsWith(".js")
+);
 for (const file of cmdFiles) {
-  const command = require(join(__dirname, "commands", file))
-  client.commands.set(command.name, command)
+  const command = require(join(__dirname, "commands", file));
+  client.commands.set(command.name, command);
 } //LOADING DONE
-
 
 //WHEN SOMEONE MESSAGE
 client.on("message", message => {
-   if (message.author.bot) return;
+  if (message.author.bot) return;
   if (!message.guild) return;
-  
-  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
-    
-    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+
+  if (message.content.startsWith(PREFIX)) {
+    //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+
+    const args = message.content
+      .slice(PREFIX.length)
+      .trim()
+      .split(/ +/); //removing prefix from args
     const command = args.shift().toLowerCase();
-    
-    if(!client.commands.has(command)) {
+
+    if (!client.commands.has(command)) {
       return;
-    } 
-    
-  try  { //TRY TO GET COMMAND AND EXECUTE
-      client.commands.get(command).execute(client, message, args)
-    //COMMAND LOGS
-    console.log(`${message.guild.name}: ${message.author.tag} Used ${client.commands.get(command).name} in #${message.channel.name}`)
-    } catch (err) { //IF IT CATCH ERROR
-      console.log(err)
-      message.reply("I am getting error on using this command")
     }
-    
+
+    try {
+      //TRY TO GET COMMAND AND EXECUTE
+      client.commands.get(command).execute(client, message, args);
+      //COMMAND LOGS
+      console.log(
+        `${message.guild.name}: ${message.author.tag} Used ${
+          client.commands.get(command).name
+        } in #${message.channel.name}`
+      );
+    } catch (err) {
+      //IF IT CATCH ERROR
+      console.log(err);
+      message.reply("I am getting error on using this command");
+    }
   }
-  
-  
 });
 
-
-
-
 //DONT DO ANYTHING WITH THIS TOKEN lol
-client.login(TOKEN)
+client.login(TOKEN);
