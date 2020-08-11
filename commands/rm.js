7679d21ffb4bbd7921b594bc93e6e54df44ce806
Glitch (music-tuto) <none>
2020-08-11 13:44:41 +0000

    👼🌑 Checkpoint ./system/music.js:20970236/3626 ./commands/drop.js:20970236/2205 ./commands/stop.js:20970236/3228 ./package.json:20970236/1901

--- commands/rm.js
+++ commands/rm.js
@@ -1,8 +1,8 @@
 const { MessageEmbed } = require("discord.js");
 const { COLOR } = require("../config.json");
 module.exports = {
-  name: "drop",
-  description: "Drop The Song From Queue",
+  name: "rm",
+  description: "Remove The Song From Queue",
   execute(client, message, args) {
     let embed = new MessageEmbed().setColor(COLOR);
     const { channel } = message.member.voice;
@@ -17,21 +17,20 @@ module.exports = {
       embed.setAuthor("The Queue is empty");
       return message.channel.send(embed);
     }
-    
-     if(isNaN(args[0])) {
-      embed.setAuthor("Please Use Numerical Values Only")
-      return message.channel.send(embed)
+
+    if (isNaN(args[0])) {
+      embed.setAuthor("Please Use Numerical Values Only");
+      return message.channel.send(embed);
     }
-   
-    if(args[0] > serverQueue.songs.length) {
-      embed.setAuthor("Unable to find this song")
-      return message.channel.send(embed)
+
+    if (args[0] > serverQueue.songs.length) {
+      embed.setAuthor("Unable to find this song");
+      return message.channel.send(embed);
     }
-    
-    
-    serverQueue.songs.splice(args[0] - 1, 1)
-    embed.setDescription("DROPED THE SONG FROM QUEUE")
-    embed.setThumbnail(client.user.displayAvatarURL())
-    return message.channel.send(embed)
+
+    serverQueue.songs.splice(args[0] - 1, 1);
+    embed.setDescription("DROPED THE SONG FROM QUEUE");
+    embed.setThumbnail(client.user.displayAvatarURL());
+    return message.channel.send(embed);
   }
 };
--- system/music.js
+++ system/music.js
@@ -1,21 +1,18 @@
 //I WILL BE BACK AFTER 5 min
 const ytdlDiscord = require("ytdl-core-discord");
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 const { QUEUE_LIMIT, COLOR } = require("../config.json");
 
 module.exports = {
   async play(song, message) {
     const queue = message.client.queue.get(message.guild.id);
-let embed = new MessageEmbed()
-.setColor(COLOR);
+    let embed = new MessageEmbed().setColor(COLOR);
 
     if (!song) {
       queue.channel.leave();
       message.client.queue.delete(message.guild.id);
-      embed.setAuthor("MUSIC QUEUE IS ENDED NOW :/")
-      return queue.textChannel
-        .send(embed)
-        .catch(console.error);
+      embed.setAuthor("Queue Ended, There is nothing more to play");
+      return queue.textChannel.send(embed).catch(console.error);
     }
 
     try {
@@ -48,11 +45,12 @@ let embed = new MessageEmbed()
         }
       })
       .on("error", console.error);
-  
+
     dispatcher.setVolumeLogarithmic(queue.volume / 100); //VOLUME
-embed.setAuthor("Started Playing Song", message.client.user.displayAvatarURL())
-    .setDescription(`**[${song.title}](${song.url})**`)
-    
+    embed
+      .setAuthor("Started Playing Song", message.client.user.displayAvatarURL())
+      .setDescription(`**[${song.title}](${song.url})**`);
+
     queue.textChannel
       .send(embed)
       .catch(err => message.channel.send("UNABLE TO PLAY SONG"));
