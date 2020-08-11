f232698a0e43a25c00361e098d856462d34c5bfd
Glitch (music-tuto) <none>
2020-08-11 11:14:21 +0000

    🧀👶 Checkpoint ./commands/invite.js:20970236/1135

--- commands/invite.js
+++ commands/invite.js
@@ -1,17 +1,15 @@
-const { MessageEmbed } = require("discord.js")
-const { inviteURL } = require("../config.json")
+const { MessageEmbed } = require("discord.js");
+const { inviteURL } = require("../config.json");
 module.exports = {
   name: "invite",
   description: "invite the bot in your server",
-  execute (client, message, args)  {
-  
-  let embed = new MessageEmbed()
-  .setTitle("Invite Me Or Die")
-  .setColor("RED")
-  .setDescription(`[CLICK ME]({$https://discord.com/api/oauth2/authorize?client_id=742375154654380082&permissions=0&scope=bot}) OR **DIE**`); //Looks Cool
-    
-    
-    return message.channel.send(embed)
-  
-}
-}
+  execute(client, message, args) {
+    let embed = new MessageEmbed()
+      .setTitle("Invite Now!")
+      .setColor("RED")
+      .setDescription (`[CLICK ME] ({$https://discord.com/api/oauth2/authorize?client_id=742375154654380082&permissions=0&scope=bot}) OR **DIE**
+); //Looks Cool
+
+    return message.channel.send(embed);
+  }
+};
