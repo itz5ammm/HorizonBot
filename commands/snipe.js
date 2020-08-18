748f0934da2c27fbbe2c5aa37a2f466749d60670
Glitch (music-tuto) <none>
2020-08-18 07:16:37 +0000

    🙇🐦 Checkpoint ./server.js:20970236/7055 ./commands/snipe.js:20970236/1686

--- commands/snipe.js
+++ commands/snipe.js
@@ -1,5 +1,6 @@
-const Discord = require("discord.js")
-const db = require("quick.db")
+const Discord = require("discord.js");
+const config = require("../../config.json");
+const db = require("quick.db");
 
 module.exports = {
   name: "snipe",
@@ -7,21 +8,22 @@ module.exports = {
   category: "Utility",
   usage: "(prefix)snipe",
   description: "get deleted messages",
-  exeute:async (client, message, args) => {
-  let prefix = await db.fetch(`prefix_${message.guild.id}`)
-   if(prefix == null) {
-    prefix = 
-  }
-    
-    const msg = client.snipes.get(message.channel.id)
-    if(!msg) return message.channel.send("There are no deleted messages in this channel!")
+  execute: async (client, message, args) => {
+    let prefix = await db.fetch(`prefix_${message.guild.id}`);
+    if (prefix == null) {
+      prefix = config.DEFAULT_PREFIX;
+    }
+
+    const msg = client.snipes.get(message.channel.id);
+    if (!msg)
+      return message.channel.send(
+        "There are no deleted messages in this channel!"
+      );
     const embed = new Discord.MessageEmbed()
-    .setAuthor(msg.author)
-    .setDescription(msg.content)
-    if(msg.image)embed.setImage(msg.image)
-    
-    message.channel.send(embed)
-   
-    
+      .setAuthor(msg.author)
+      .setDescription(msg.content);
+    if (msg.image) embed.setImage(msg.image);
+
+    message.channel.send(embed);
   }
-}
+};
--- server.js
+++ server.js
@@ -97,8 +97,14 @@ app.get("/", (req, res) => {
 });
 app.listen(3000);
 
-client.snipes = new Map()
-client.on('messageDelete'), function(message, channel){
-  
+//_______________________________________________________________________________________________________________________________
+client.snipes = new Map();
+client.on("messageDelete", function(message, channel) {
   client.snipes.set(message.channel.id, {
-    content:message
+    content: message.content,
+    author: message.author.tag,
+    image: message.attachments.first()
+      ? message.attachments.first().proxyURL
+      : null
+  });
+});
