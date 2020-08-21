c0a5fe1488cac7f1635b92df5580095ed27d4cfc
Glitch (music-tuto) <none>
2020-08-21 19:31:32 +0000

    ⭐️👼 Checkpoint ./commands/volume.js:20970236/8743

---
+++ commands/volume.js
@@ -0,0 +1,50 @@
+const { MessageEmbed } = require("discord.js");
+
+const { COLOR } = require("../config.json");
+module.exports = {
+  name: "volume",
+  category: "Music",
+  description: "Mᴀɴᴀɢᴇ Tʜᴇ Vᴏʟᴜᴍᴇ.",
+  execute(client, message, args) {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+      return message.channel.send("Yᴏᴜ Aʀᴇ Nᴏᴛ Aʟʟᴏᴡᴇᴅ Tᴏ Cʜᴀɴɢᴇ Tʜᴇ Vᴏʟᴜᴍᴇ.");
+    }
+
+    let embed = new MessageEmbed().setColor(COLOR);
+
+    const { channel } = message.member.voice;
+    if (!channel) {
+      //IF AUTHOR IS NOT IN VOICE CHANNEL
+      embed.setAuthor("YOU NEED TO BE IN VOICE CHANNEL");
+      return message.channel.send(embed);
+    }
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+
+    if (!serverQueue) {
+      embed.setAuthor("Bᴏᴛ Is Nᴏᴛ Pʟᴀʏɪɴɢ Aɴʏᴛʜɪɴɢ.");
+      return message.channel.send(embed);
+    }
+
+    if (!args[0]) {
+      embed.setAuthor(`Tʜᴇ Cᴜʀʀᴇɴᴛ Vᴏʟᴜᴍᴇ Is ${serverQueue.volume}`);
+      return message.channel.send(embed);
+    }
+
+    if (isNaN(args[0])) {
+      embed.setAuthor("Pʟᴇᴀsᴇ Usᴇ Nᴜᴍᴇʀɪᴄᴀʟ Vᴀʟᴜᴇ Oɴʟʏ.");
+      return message.channel.send(embed);
+    }
+
+    if (args[0] > 200) {
+      embed.setAuthor("Yᴏᴜ Cᴀɴᴛ Iɴᴄʀᴇᴀsᴇ Vᴏʟᴜᴍᴇ Mᴏʀᴇ Tʜᴀɴ 200");
+      return message.channel.send(embed);
+    }
+
+    serverQueue.volume = args[0];
+    serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
+    embed.setDescription(`Sᴇᴛ Vᴏʟᴜᴍᴇ Tᴏ ${args[0]}`);
+    embed.setThumbnail(client.user.displayAvatarURL());
+    message.channel.send(embed);
+  }
+};
