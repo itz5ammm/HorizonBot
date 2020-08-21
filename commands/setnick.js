76f2d4921327e1264338a260faeb5c699be96538
Glitch (music-tuto) <none>
2020-08-21 17:52:46 +0000

    👨🏗 Checkpoint ./commands/setnick.js:20970236/2282

---
+++ commands/setnick.js
@@ -0,0 +1,23 @@
+const discord = require("discord.js");
+const db = require("quick.db");
+
+module.exports = {
+  name: "setnick",
+  category: "Moderation",
+  aliases: ["setnickname", "nick"],
+  description: "Cʜᴀɴɢᴇ Sᴏᴍᴇᴏɴᴇ's Nɪᴄᴋɴᴀᴍᴇ.",
+  execute: async (client, message, args) => {
+    let perm = message.member.hasPermission("ADMINISTRATOR");
+    if (!perm) return message.channel.send("No perms");
+
+    let user = message.mentions.members.first();
+    if (!user) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ!");
+
+    let name = args.slice(1).join(" ");
+    if (!name) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Nᴀᴍᴇ!");
+
+    user.setNickname(name);
+
+    message.channel.send(`${user.user.tag}'s name have changed to ${name}`);
+  }
+};
