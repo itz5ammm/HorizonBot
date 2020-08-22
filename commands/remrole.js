17457de85bcfc90aad0bab66100fcb5a3470756a
Glitch (music-tuto) <none>
2020-08-22 11:27:40 +0000

    🌦🚿 Checkpoint ./commands/remrole.js:20970236/2592

---
+++ commands/remrole.js
@@ -0,0 +1,29 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "remrole",
+  category: "Moderation",
+  aliases: ["remroles"],
+  description: "Add a role to a user",
+  usage: "<user> <role>",
+  execute: async (client, message, args) => {
+    let perm = message.member.hasPermission("MANAGE_ROLES");
+    if (!perm) return message.channel.send("Yᴏᴜ Nᴇᴇᴅ Mᴀɴᴀɢᴇ Rᴏʟᴇ Pᴇʀᴍs.");
+
+    let user = message.mentions.members.first();
+    if (!user) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ.");
+
+    let role = message.mentions.roles.first();
+    if (!role) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Rᴏʟᴇ.");
+
+    if (!user.roles.has(role))
+      return message.channel.send("Tʜɪs Usᴇʀ Dᴏᴇs Nᴏᴛ Hᴀᴠᴇ Tʜᴇ Rᴏʟᴇ.");
+
+    let embed = new discord.MessageEmbed()
+      .setColor("00FFFF")
+      .setDescription(`${user}, -${role}`);
+
+    user.roles.remove(role);
+    message.channel.send(embed);
+  }
+};
