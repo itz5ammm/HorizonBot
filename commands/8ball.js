8b76326a36a5e64818d6d176018db38750a7205c
Glitch (music-tuto) <none>
2020-08-22 11:16:02 +0000

    🛥⛽️ Checkpoint ./commands/addrole.js:20970236/2302

---
+++ commands/8ball.js
@@ -0,0 +1,26 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "8ball",
+  category: "Fun",
+  aliases: ["eightball"],
+  description: "Asᴋ Mᴇ A Qᴜᴇsᴛɪᴏɴ I'ʟʟ Aɴsᴡᴇʀ Iᴛ.",
+  usage: "<question>",
+  execute: async (client, message, args) => {
+    let replies = ["yes", "no", "maybe", "ask someone else", "i don't know"];
+
+    let question = args.slice(0).join(" ");
+    const embed1 = new discord.MessageEmbed()
+      .setColor("00FFFF")
+      .setDescription("Asᴋ Mᴇ Tʜᴇ Qᴜᴇsᴛɪᴏɴ.");
+    if (!question) return message.channel.send(embed1);
+
+    let result = Math.floor(Math.random() * replies.length);
+
+    let embed = new discord.MessageEmbed()
+      .setColor("#00FFFF")
+      .addField("Yᴏᴜʀ Qᴜᴇsᴛɪᴏɴ", `${question}`)
+      .addField("Mʏ Aɴsᴡᴇʀ", `${replies[result]}`);
+    message.channel.send(embed);
+  }
+};
---
+++ commands/addrole.js
@@ -0,0 +1,26 @@
+const discord = require("discord.js");
+
+module.exports = {
+  name: "addrole",
+  category: "Moderation",
+  aliases: ["addroles"],
+  description: "Aᴅᴅ Rᴏʟᴇ Tᴏ A Usᴇʀ.",
+  usage: "<user> <role>",
+  execute: async (client, message, args) => {
+    let perm = message.member.hasPermission("MANAGE_ROLES");
+    if (!perm) return message.channel.send("Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Usᴇ Tʜɪs.");
+
+    let user = message.mentions.members.first();
+    if (!user) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ.");
+
+    let role = message.mentions.roles.first();
+    if (!role) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Rᴏʟᴇ.");
+
+    let embed = new discord.MessageEmbed()
+      .setColor("00FFFF")
+      .setDescription(`${user}, +${role}`);
+
+    user.roles.add(role);
+    message.channel.send(embed);
+  }
+};
