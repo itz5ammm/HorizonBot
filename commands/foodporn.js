c68d7d5e5b941194f4c239517df61b02570d5938
Glitch (music-tuto) <none>
2020-08-22 11:38:49 +0000

    🚆🎸 Checkpoint ./commands/foodporn.js:20970236/2214 ./package.json:20970236/2237

---
+++ commands/foodporn.js
@@ -0,0 +1,26 @@
+const discord = require("discord.js");
+const got = require("got");
+
+module.exports = {
+  name: "food",
+  category: "Fun",
+  aliases: ["foodporn"],
+  description: "Sends a random yummy food picture",
+  usage: "food",
+  execute: async (client, message, args) => {
+    got("https://www.reddit.com/r/foodporn/random/.json")
+      .then(response => {
+        let content = JSON.parse(response.body);
+        let memeImage = content[0].data.children[0].data.url;
+        let memeTitle = content[0].data.children[0].data.title;
+
+        const MemeEmbed = new discord.MessageEmbed()
+          .setColor("RANDOM")
+          .setTitle(`${memeTitle}`)
+          .setImage(memeImage);
+
+        message.channel.send(MemeEmbed);
+      })
+      .catch(console.error);
+  }
+};
