724aab1e65058e2a9642dc1f11c4f63818759ca5
Glitch (music-tuto) <none>
2020-08-23 05:21:21 +0000

    📼🍇 Checkpoint ./commands/ping.js:20970236/834

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,12 @@ module.exports = {
   name: "ping",
   description: "Pɪɴɢɪɴɢ Tʜᴇ Bᴏᴛ.",
   category: "Utility",
-  execute(client, message) {
-    message.channel.send(`Pᴏɴɢ! ${client.ws.ping}ms`);
+  execute(client, message, args) {
+  
+    let embed = new MessageEmbed()
+    .setColor("00FFFF")
+    .setDescription("Pᴏɴɢ! ${client.ws.ping}ms.")
+    
+    return message.channel.send(embed);
   }
 };
