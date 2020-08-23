7c10600087567c643028b938e0b9603b1e19c995
Glitch (music-tuto) <none>
2020-08-23 06:08:21 +0000

    🚙🌎 Checkpoint ./commands/ping.js:20970236/938

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,11 @@
 //FIRST TEST HANDLER IS WORKING OR NOT
 module.exports = {
   name: "ping",
-  description: "Pinging the bot",
+  description: "Pɪɴɢɪɴɢ Tʜᴇ Bᴏᴛ",
   execute(client, message) {
-    let diff = (Date.now() - start);
-    let API = Math.round((bot.ping).toFixed())
     let embed = new MessageEmbed()
     .setTitle(`Pᴏɴɢ!`)
     .setColor(`00FFFF`)
-    .addField("Lᴀᴛᴇɴᴄʏ", `${diff}ms`, true)
-    .addField("API", `${API}ms`, true)
-    .setFooter("Rᴇǫᴜᴇsᴇᴅ Bʏ: `${message.author.tag}
+    .setDescription(`
   }
 };
