e1e0629adf56efb65de80de68ce6862bb21a0684
Glitch (music-tuto) <none>
2020-08-23 15:12:39 +0000

    🍉🍉 Checkpoint ./server.js:20970236/1161 ./commands/snipe.js:20970236/2382

--- server.js
+++ server.js
@@ -97,4 +97,14 @@ app.get("/", (req, res) => {
 });
 app.listen(3000);
 
-//_______________________________________________________________________________________________________________________________
+//______________________________________________________________________________________________________________________________
+client.snipes = new Map()
+client.on('messageDelete', function(message, channel){
+  
+  client.snipes.set(message.channel.id, {
+    content:message.content,
+    author:message.author.tag,
+    image:message.attachments.first() ? message.attachments.first().proxyURL : null
+  })
+  
+})
