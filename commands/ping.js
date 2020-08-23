ac46dcb48b17aa7ed6992f54bf05e094007c8b18
Glitch (music-tuto) <none>
2020-08-23 05:45:18 +0000

    🏁🎉 Checkpoint ./commands/ping.js:20970236/2101

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,8 @@
 //FIRST TEST HANDLER IS WORKING OR NOT
 module.exports = {
   name: "ping",
-  description: "Pɪɴɢɪɴɢ Tʜᴇ Bᴏᴛ.",
-  category: "Utility",
-  execute(client, message, args) {
-  
-let embed = new MessageEmbed()
-    .setColor("00FFFF")
-    .setDescription(`🎶 Pᴏɴɢ! ${client.ws.ping}ms.`)
-    .setFooter(`Requested By: ${message.author.tag}`)
-    
-return message.channel.send(embed);
+  description: "Pinging the bot",
+  execute(client, message) {
+    message.channel.send("PONG ");
   }
 };
