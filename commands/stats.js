7325ac46292653a26ae1d942d43828eca2a47217
Glitch (music-tuto) <none>
2020-08-24 18:19:28 +0000

    👽🎥 Checkpoint ./commands/stats.js:20970236/442

--- commands/stats.js
+++ commands/stats.js
@@ -23,20 +23,6 @@ module.exports = {
       .addField("Tᴏᴛᴀʟ Usᴇʀs", client.users.cache.size, true)
       .addField("Nᴏ. Oғ Gᴜɪʟᴅ Iɴ Bᴏᴛ Is Pʟᴀʏɪɴɢ", client.queue.size)
       .addField("Bᴏᴛ Cʀᴇᴀᴛᴇᴅ Oɴ", client.user.createdAt)
-      .addField("Dᴇᴠᴇᴏᴘᴇʀs", const Discord = require("discord.js");
-
-module.exports = { 
-name: "botinfo",
-category: "Utility",
-description: "Get Bot Info",
-execute: async (bot, message, args) => {
-    let inline = true
-    let bicon = bot.user.displayAvatarURL;
-    let usersize = bot.users.size
-    let chansize = bot.channels.size
-    let uptimxd = bot.uptime 
-    let servsize = bot.guilds.size
-    let botembed
       .setFooter(`Requested By: ${message.author.tag}`)
       .setTimestamp();
     console.log(client.user.presence);
