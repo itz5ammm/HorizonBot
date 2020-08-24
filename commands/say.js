828ff9eeecfdd910d13e826239d03109972c3f87
Glitch (music-tuto) <none>
2020-08-24 17:44:06 +0000

    🍜🏏 Checkpoint ./commands/embed.js:20970236/92 ./commands/say.js:20970236/404

---
+++ commands/say.js
@@ -0,0 +1,16 @@
+module.exports = {
+name: "say",
+category: "Fun",
+description: "Say The Message Through The Bot",
+usage: "say <text>",
+execute: async (bot, message, args) => {
+    
+    if(message.author.id === "448410421339095041") return
+    if(message.author.id === "485494651835383828") return
+    
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+
+}
+}
