93c92b2d551039c5406a4ba48956c0353361adec
Glitch (music-tuto) <none>
2020-08-25 09:56:14 +0000

    👩‍❤️‍💋‍👩💂 Checkpoint ./commands/setnick.js:20970236/1926 ./config.json:20970236/780

--- commands/setnick.js
+++ commands/setnick.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js");
 const db = require("quick.db");
-const { Embed } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "setnick",
@@ -9,7 +9,7 @@ module.exports = {
   description: "Cʜᴀɴɢᴇ Sᴏᴍᴇᴏɴᴇ's Nɪᴄᴋɴᴀᴍᴇ.",
   execute: async (client, message, args) => {
     let perm = message.member.hasPermission("MANAGE_NICKNAMES");
-    if (!perm) return message.channel.send("Yᴏᴜ Dᴏɴᴛ Hᴀᴠᴇ Pᴇʀᴍs ᴏ ᴏ Tʜᴀᴛ");
+    if (!perm) return message.channel.send("Yᴏᴜ Dᴏɴᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Usᴇ Tʜᴀᴛ");
 
     let user = message.mentions.members.first();
     if (!user) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ!");
@@ -19,6 +19,10 @@ module.exports = {
 
     user.setNickname(name);
 
-    message.channel.send(`Sᴇᴛ Nɪᴄᴋɴᴀᴍᴇ Fᴏʀ ${user.user.tag}.`);
+    let embed = new MessageEmbed()
+      .setColor("070707")
+      .setDescription(`Sᴇᴛ Nɪᴄᴋɴᴀᴍᴇ Fᴏʀ ${user.user.tag}.`);
+
+    message.channel.send(embed);
   }
 };
