a7803ea9a0bb58670cc78ad36d770bdb1cfe8ee4
Glitch (music-tuto) <none>
2020-08-25 06:04:04 +0000

    🚉👘 Checkpoint ./commands/poll.js:20970236/2968

--- commands/poll.js
+++ commands/poll.js
@@ -13,9 +13,11 @@ module.exports = {
       ); //If he doesn't specify a text it will stop and tell him to type a text for the poll
 
     const embed = new discord.MessageEmbed() //Creating an embed because it looks better for everything and just cleaner.
-      .setTitle("Gᴇᴛᴛɪɴɢ Sᴛᴀʀᴛᴇᴅ!") //This is a title and it is a "New Poll", you can cange it if you'd like or remove it
+      .setTitle("Aʙᴏᴜᴛ Bᴏᴛ.") //This is a title and it is a "New Poll", you can cange it if you'd like or remove it
       .setColor("00ffff")
-      .setDescription(`${text}`); //This is where the text will be sent, i would like to remind you that in embed titles and in titles and in footers you cannot have any mentions thats why im using a description
+      .setDescription(`${text}`) //This is where the text will be sent, i would like to remind you that in embed titles and in titles and in footers you cannot have any mentions thats why im using a description
+      .setFooter(`Mᴀᴅᴇ Wɪᴛʜ ❤️ Oғ Dᴇᴠᴇʟᴏᴘᴇʀs.`)
+      .setTimestamp();
 
     let pollmessage = await message.channel.send(embed); //Using await so after the message is sent to be able to be able to do actions to it
 
