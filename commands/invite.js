95739ab272462773ae152f42793f248885e98a3d
Glitch (music-tuto) <none>
2020-08-25 10:54:28 +0000

    🎱🐙 Checkpoint ./commands/invite.js:20970236/1470

--- commands/invite.js
+++ commands/invite.js
@@ -7,9 +7,14 @@ module.exports = {
   execute(client, message, args) {
     let embed = new MessageEmbed()
       .setTitle("Aᴅᴅ Tʜᴇ Bᴏᴛ Tᴏ Yᴏᴜʀ Sᴇʀᴠᴇʀ!")
-      .setColor("RED")
+      .setColor("BLACK")
       .setDescription(
         `[Invite Here](https://discord.com/api/oauth2/authorize?client_id=742375154654380082&permissions=8&scope=bot)`,
+        true
+      )
+      .addField(
+        "Jᴏɪɴ Sᴘᴘᴏʀᴛ Sᴇʀᴠᴇʀ!",
+        `[Join Here](https://discord.gg/UuXQMBm)`,
         true
       );
 
