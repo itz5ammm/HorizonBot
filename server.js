0b7177e79495a1d52ed158979573fcc09b43c03b
Glitch (music-tuto) <none>
2020-08-25 10:07:18 +0000

    🏊🏼🙅 Checkpoint ./server.js:20970236/1952 ./commands/blacklist.js:20970236/1428

--- server.js
+++ server.js
@@ -87,6 +87,40 @@ client.on("message", message => {
   }
 });
 
+
+client.on("message", async message => {
+   let prefix = await db.fetch(`prefix_${message.guild.id}`)
+   if(prefix == null) {
+    prefix = "+" 
+  }
+  
+     let blacklist = await db.fetch(`blacklist_${message.author.id}`)
+   
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+      
+    if (blacklist === "Blacklisted") return message.reply("You are blacklisted from the bot!")
+
+    // If message.member is uncached, cache it.
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    // Get the command
+    let command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    // If a command is finally found, run the command
+    if (command) 
+        command.run(client, message, args);
+  
+});
+
 //DONT DO ANYTHING WITH THIS TOKEN lol
 client.login(TOKEN);
 
