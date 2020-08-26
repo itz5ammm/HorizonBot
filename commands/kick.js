497397da7a7a06be0dc007ced689bd5171aeb0b9
Glitch (music-tuto) <none>
2020-08-26 05:45:23 +0000

    🙈👳 Checkpoint ./commands/kick.js:20970236/6104

--- commands/kick.js
+++ commands/kick.js
@@ -1,46 +1,60 @@
 const discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "kick",
-  category: "Moderation",
-  aliases: ["kickuser"],
-  description: "Kɪᴄᴋ A Usᴇʀ Fʀᴏᴍ Tʜᴇ Sᴇʀᴠᴇʀ.",
-  usage: "<user> (reason)",
-  execute: async (client, message, args) => {
-    if (!message.member.hasPermission("KICK_MEMBERS"))
-      return message.channel.send("Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Usᴇ Tʜɪs.");
+  category: "moderation",
+  description: "Kick Anyone From The Server.",
+  usage: "kick <@user> <reason>",
+  execute: (client, message, args) => {
+    if (!message.member.hasPermission("KICK_MEMBERS")) {
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription(
+          `**${message.author.tag}**, You Don't Have Enough Perms To Kick Someone.`
+        );
 
-    let logchannel = message.guild.channels.cache.find(
-      ch => ch.name === "horizon-modlogs"
-    );
-    if (!logchannel) return message.channel.send("Cᴀɴ'ᴛ Fɪɴᴅ Tʜᴇ Lᴏɢ Cʜᴀɴɴᴇʟ.");
-
-    let user = message.mentions.members.first();
-    if (!user) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ.");
-
-    let reason = args.slice(1).join(" ");
-    if (!reason) reason = "Pʀᴏᴠɪᴅᴇ Tʜᴇ Rᴇᴀsᴏɴ.";
-
-    if (user.hasPermission("KICK_MEMBERS"))
-      return message.channel.send(
-        "Yᴏᴜ Cᴀɴᴛ Kɪᴄᴋ Sᴏᴍᴇᴏɴᴇ Wʜᴏ Hᴀs Tʜᴇ Sᴀᴍᴇ/Hɪɢʜᴇʀ Pᴇʀᴍs."
-      );
-    let logembed = new discord.MessageEmbed()
-      .setColor("00FFFF")
-      .setTitle(`Usᴇʀ Kɪᴄᴋᴇᴅ | ${user.user.tag}`)
-      .addField("Sᴛᴀғғ", `${message.author}`)
-      .addField("Rᴇᴀsᴏɴ", `${reason}`);
-
-    try {
-      user.send(
-        `Yᴏᴜ Hᴀᴠᴇ Bᴇᴇɴ Wᴀʀɴᴇᴅ Iɴ ${message.guild.name} Bʏ ${message.author} Fᴏʀ ${reason}.`
-      );
-    } catch (err) {
-      console.log(err);
+      message.channel.send(embed);
     }
 
-    await user.kick();
-    logchannel.send(logembed);
-    message.channel.send(`${user} Wᴀs Kɪᴄᴋᴇᴅ.`);
+    if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription(
+          `**${message.author.tag}**, I Don't Have Enough Perms.`
+        );
+
+      message.channel.send(embed);
+    }
+
+    let target = message.mentions.members.first();
+
+    if (!target) {
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription(
+          `**${message.author.tag}**, Please mention the person who you want to kick.`
+        );
+
+      message.channel.send(embed);
+    }
+
+    if (target.id === message.author.id) {
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription(`**${message.author.tag}**, You Can't Kick Yourself.`);
+
+      message.channel.send(embed);
+    }
+
+    let embed = new discord.MessageEmbed()
+      .setTitle("Action: Kick")
+      .setDescription(`***${target.user.tag} was Kicked.***`)
+      .setColor("#070707")
+      .setFooter(`Kicked by ${message.author.tag}`);
+
+    message.channel.send(embed);
+
+    target.kick(args[1]);
   }
 };
