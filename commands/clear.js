c841d90fb534a158cdfd7c389a01b21828046aaa
Glitch (music-tuto) <none>
2020-08-28 14:53:28 +0000

    🏖🎙 Checkpoint ./commands/clear.js:20970236/5521

--- commands/clear.js
+++ commands/clear.js
@@ -1,32 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
   name: "clear",
+  description: "Clears Messages Of Mentioned Amount.",
   category: "Moderation",
-  description: "Clears Messages.",
-  usage: "clear <amount>",
-  execute: async (client, msg, args) => {
-    let [userId, limit] = args.split(/\s+/);
-    if (!userId && !limit) {
-      let deletedMessages = await msg.channel.bulkDelete();
-      msg.channel.send(`${deletedMessages.size} messages were deleted.`);
-    }
-    if (!userId || !limit)
-      return msg.channel.send("Please provide the correct arguments.");
-    let r = new RegExp(/^\d+$/);
-    if (!r.test(userId))
-      return msg.channel.send("Please provide a valid user id.");
-    if (isNaN(limit))
-      return msg.channel.send("Please provide a numeric value for limit");
-    if (limit > 100)
-      return msg.channel.send("Limit must be less than or equal to 100.");
-    try {
-      let fetchedMessages = await msg.channel.messages.fetch({ limit });
-      let filteredMessages = fetchedMessages.filter(
-        msg => msg.author.id === userId
-      );
-      let deletedMessages = await msg.channel.bulkDelete(filteredMessages);
-      msg.channel.send(`${deletedMessages.size} messages were deleted.`);
-    } catch (err) {
-      console.log(err);
+  execute: async (client, message, args) => {
+    const deleteCount = parseInt(args[0], 10);
+
+    // get the delete count, as an actual number.
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription("Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Usᴇ Tʜɪs.");
+      message.channel.send(embed);
+    } else {
+      // Ooooh nice, combined conditions. <3
+      if (!deleteCount || deleteCount < 2 || deleteCount > 100) {
+        let embed = new MessageEmbed()
+          .setColor("070707")
+          .setDescription("Pʀᴏᴠɪᴅᴇ Tʜᴇ Nᴜᴍʙᴇʀ Oғ Mᴇssᴀɢᴇs.");
+        message.channel.send(embed);
+      }
+
+      await message.channel.bulkDelete(deleteCount).catch(console.log);
     }
   }
 };
