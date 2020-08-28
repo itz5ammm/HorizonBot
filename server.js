4d24b7c5fbbf112f4ee14f6bceade81f866c2f89
Glitch (tall-held-freon) <none>
2020-08-28 17:53:19 +0000

    Rewound to commit f618d6a56b3f82cabb5fffe2e22a18120d092d4a + 0 edits

--- server.js
+++ server.js
@@ -1,63 +1,151 @@
-const discord = require("discord.js")
-const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const discord = require("discord.js"); // fixed enjoi
+const client = new discord.Client({
+  disableEveryone: true,
+  disabledEvents: ["TYPING_START"]
+});
 const { readdirSync } = require("fs");
 const { join } = require("path");
-const { TOKEN, PREFIX } = require("./config.json")
+const { TOKEN, PREFIX } = require("./config.json");
 
 //CLIENT EVENTS
 client.on("ready", () => {
-  console.log('Ready to play song | Bot created by CTK WARRIOR')
-  client.user.setActivity("+help | Musix")
-})
+  console.log("Ready to play song | Bot created by SAM");
+  client.user.setActivity("+help | Horizon - A Cᴏᴏʟ Mᴜʟᴛɪ-ᴘᴜʀᴘᴏsᴇ Bᴏᴛ.");
+});
 
 client.on("warn", info => console.log(info));
 
-client.on("error", console.error)
+client.on("error", console.error);
 
 //DEFINIING
-client.commands = new discord.Collection()
-client.prefix = PREFIX
+client.commands = new discord.Collection();
+client.prefix = PREFIX;
 client.queue = new Map();
 client.vote = new Map();
+client.capitalize = string => {
+  let str = "";
+  string = string.split(" ");
+  for (let i = 0; i < string.length; i++) {
+    str +=
+      string[i].charAt(0).toUpperCase() +
+      string[i].slice(1).toLowerCase() +
+      " ";
+    if (i == string.length - 1) {
+      string = str.split("-");
+      str = "";
+      for (let i = 0; i < string.length; i++) {
+        str += string[i].charAt(0).toUpperCase() + string[i].slice(1) + "-";
+        if (i == string.length - 1) {
+          return str.slice(0, -2);
+        }
+      }
+    }
+  }
+};
 
 //LETS LOAD ALL FILES
-const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"))
+const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file =>
+  file.endsWith(".js")
+);
 for (const file of cmdFiles) {
-  const command = require(join(__dirname, "commands", file))
-  client.commands.set(command.name, command)
+  const command = require(join(__dirname, "commands", file));
+  client.commands.set(command.name, command);
 } //LOADING DONE
-
 
 //WHEN SOMEONE MESSAGE
 client.on("message", message => {
-   if (message.author.bot) return;
+  if (message.author.bot) return;
   if (!message.guild) return;
-  
-  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
-    
-    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+
+  if (message.content.startsWith(PREFIX)) {
+    //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+
+    const args = message.content
+      .slice(PREFIX.length)
+      .trim()
+      .split(/ +/); //removing prefix from args
     const command = args.shift().toLowerCase();
-    
-    if(!client.commands.has(command)) {
+
+    if (!client.commands.has(command)) {
       return;
-    } 
-    
-  try  { //TRY TO GET COMMAND AND EXECUTE
-      client.commands.get(command).execute(client, message, args)
-    //COMMAND LOGS
-    console.log(`${message.guild.name}: ${message.author.tag} Used ${client.commands.get(command).name} in #${message.channel.name}`)
-    } catch (err) { //IF IT CATCH ERROR
-      console.log(err)
-      message.reply("I am getting error on using this command")
     }
-    
+
+    try {
+      //TRY TO GET COMMAND AND EXECUTE
+      client.commands.get(command).execute(client, message, args);
+
+      //COMMAND LOGS
+      const o = client.channels.cache.get("748919962235568330");
+      const embed = new discord.MessageEmbed()
+        .setTitle("Bot Command Used")
+        .setDescription(
+          `**${message.guild.name}** :
+**${message.author.tag}** Used: **\`${client.commands.get(command).name}\`**
+in **${message.channel.name}**`
+        )
+        .setColor("00FFFF")
+        .setThumbnail(message.author.avatarURL())
+        .setTimestamp();
+      o.send(embed);
+    } catch (err) {
+      //IF IT CATCH ERROR
+      console.log(err);
+      message.reply("Gᴇᴛᴛɪɴɢ Eʀʀᴏʀ Usɪɴɢ Tʜɪs Cᴏᴍᴍᴀɴᴅ.");
+    }
   }
-  
-  
 });
 
+{ client.commands = new Collection();
+client.aliases = new Collection();
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.snipes = new Map()
+client.on('messageDelete', function(message, channel){
+  
+  client.snipes.set(message.channel.id, {
+    content:message.content,
+    author:message.author.tag,
+    image:message.attachments.first() ? message.attachments.first().proxyURL : null
+  })
+  
+})
 
 
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(PREFIX)) return;
+
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+
+  const args = message.content
+    .slice(PREFIX.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (cmd.length === 0) return;
+
+  // Get the command
+  let command = client.commands.get(cmd);
+  // If none is found, try to find it by alias
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+  // If a command is finally found, run the command
+  if (command) command.run(client, message, args);
+});
 
 //DONT DO ANYTHING WITH THIS TOKEN lol
-client.login(TOKEN)
+client.login(TOKEN);
+
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+app.listen(3000);
+}
