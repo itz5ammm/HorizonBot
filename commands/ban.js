f586e8a16d784c2634d2b173ed59335f5e42fa3d
Glitch (music-tuto) <none>
2020-08-28 07:10:13 +0000

    ☕️🚈 Checkpoint ./commands/ban.js:20970236/4063

--- commands/ban.js
+++ commands/ban.js
@@ -1,60 +1,69 @@
 const discord = require("discord.js");
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "ban",
   category: "moderation",
   description: "Ban anyone with one shot whithout knowing anyone xD",
   usage: "ban <@user> <reason>",
   execute: async (client, message, args) => {
-    
-    if(!message.member.hasPermission("BAN_MEMBERS")) {
-      return 
+    if (!message.member.hasPermission("BAN_MEMBERS")) {
+      return;
       let embed = new MessageEmbed()
-      .setColor("070707")
-      .setDescription(`**${message.author.tag}**, Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Bᴀɴ Sᴏᴍᴇᴏɴᴇ.`)
+        .setColor("070707")
+        .setDescription(
+          `**${message.author.tag}**, Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Bᴀɴ Sᴏᴍᴇᴏɴᴇ.`
+        );
     }
-    
-    if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
-      return 
+
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
+      return;
       let embed = new MessageEmbed()
-      .setColor("070707")
-      .setDescription(`**${message.author.tag}**, Mᴀᴋᴇ Sᴜʀᴇ I Hᴀᴠᴇ Tʜᴇ Rᴇǫᴜɪʀᴇᴅ Pᴇʀᴍs.`)
-    }
-    
-    const target = message.mentions.members.first();
-    
-    const reason = args.slice(1).join(" ");
-    
-    if(!target) {
-      return 
-      let embed = new MessageEmbed()
-      .setColor("070707")
-      .setDescription(`**${message.author.username}**, Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ Yᴏᴜ Wᴀɴᴛ Tᴏ Bᴀɴ.`)
- 
+        .setColor("070707")
+        .setDescription(
+          `**${message.author.tag}**, Mᴀᴋᴇ Sᴜʀᴇ I Hᴀᴠᴇ Tʜᴇ Rᴇǫᴜɪʀᴇᴅ Pᴇʀᴍs.`
+        );
+
       message.channel.send(embed);
-      }
-    
-    if(target.id === message.author.id) {
-      return message.channel.send(`**${message.author.username}**, You can not ban yourself!`)
     }
-    
-   
-    
-   if(!args[1]) {
-     return message.channel.send(`**${message.author.username}**, Please Give Reason To ban Member`)
-   }
-    
+
+    const target = message.mentions.members.first();
+
+    const reason = args.slice(1).join(" ");
+
+    if (!target) {
+      return;
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription(
+          `**${message.author.tag}**, Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ Yᴏᴜ Wᴀɴᴛ Tᴏ Bᴀɴ.`
+        );
+
+      message.channel.send(embed);
+    }
+
+    if (target.id === message.author.id) {
+      return;
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription(`**${message.author.tag}**, Yᴏᴜ Cᴀɴ'ᴛ Bᴀɴ Yᴏᴜʀsᴇʟғ.`);
+
+      message.channel.send(embed);
+    }
+
+    if (!args[1]) {
+      return message.channel.send(
+        `**${message.author.username}**, Please Give Reason To ban Member`
+      );
+    }
+
     let embed = new discord.MessageEmbed()
-    .setTitle("Action : Ban")
-    .setDescription(`Banned ${target} (${target.id})`)
-    .setColor("#ff2050")
-    .setThumbnail(target.avatarURL)
-    .setFooter(`Banned by ${message.author.tag}`);
-    
-    message.channel.send(embed)
-    target.ban(args[1])
-    
-    
-    
+      .setTitle("Action : Ban")
+      .setDescription(`***${target} was Banned.*** (${target.id})`)
+      .setColor("#ff2050")
+      .setThumbnail(target.avatarURL)
+      .setFooter(`Banned by ${message.author.tag}`);
+
+    message.channel.send(embed);
+    target.ban(args[1]);
   }
-}
+};
