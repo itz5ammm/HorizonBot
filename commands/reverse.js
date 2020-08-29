7a5e2f4ad8b495c540696c7a074d32e64de8824e
Glitch (tall-held-freon) <none>
2020-08-29 15:33:53 +0000

    🚜🌪 Checkpoint ./commands/reverse.js:20970236/4126

--- commands/reverse.js
+++ commands/reverse.js
@@ -1,18 +1,26 @@
+const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "reverse",
   description: "Reverse The Text",
   category: "Fun",
   usage: "reverse <text>",
-  execute: function(bot, msg, args) {
+  execute: function(bot, message, args) {
     if (args.length < 1) {
-      throw "You must input text to be reversed!";
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription("You must input text to be reversed!");
+
+      message.channel.send(embed);
     }
-    msg.reply(
+
+    let embed = new MessageEmbed().setColor("070707").setDescription(
       args
         .join(" ")
         .split("")
         .reverse()
         .join("")
     );
+
+    message.channel.send(embed);
   }
 };
