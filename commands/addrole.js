5b9206c20bf26839963afe7083c075a3577ce734
Glitch (tall-held-freon) <none>
2020-08-29 13:18:51 +0000

    💓🙍 Checkpoint ./commands/addrole.js:20970236/4600

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,42 +1,44 @@
-const discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const checkPermissionRole = role =>
+  role.permissions.has("ADMINISTRATOR") ||
+  role.permissions.has("KICK_MEMBERS") ||
+  role.permissions.has("BAN_MEMBERS") ||
+  role.permissions.has("MANAGE_GUILD") ||
+  role.permissions.has("MANAGE_CHANNELS");
 
 module.exports = {
   name: "addrole",
   category: "Moderation",
-  aliases: ["addroles"],
-  description: "Aᴅᴅ Rᴏʟᴇ Tᴏ A Usᴇʀ.",
-  usage: "<user> <role>",
+  description: "Add Role To A User.",
+  usage: "addrole <@user> <@role>",
   execute: async (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_ROLES")) {
-      let embed = new MessageEmbed()
-        .setColor("070707")
-        .setDescription("Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Usᴇ Tʜɪs.");
-
-      message.channel.send(embed);
-    }
-    let user = message.mentions.members.first();
-    {
-      let embed = new MessageEmbed()
-        .setColor("070707")
-        .setDescription("Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ.");
-
-      message.channel.send(embed);
-    }
-
-    let role = message.mentions.roles.first();
-    {
-      let embed = new MessageEmbed()
-        .setColor("070707")
-        .setDescription("Mᴇɴᴛɪᴏɴ Tʜᴇ Rᴏʟᴇ.");
-
-      message.channel.send(embed);
-    }
-    let embed = new discord.MessageEmbed()
-      .setColor("00FFFF")
-      .setDescription(`${user}, +${role}`);
-
-    user.roles.add(role);
-    message.channel.send(embed);
+    let roleNames = args.split(", ");
+    let roleSet = new Set(roleNames);
+    let { cache } = message.guild.roles;
+    roleSet.forEach(roleName => {
+      let role = cache.find(
+        role => role.name.toLowerCase() === roleName.toLowerCase()
+      );
+      if (role) {
+        if (message.member.roles.cache.has(role.id)) {
+          message.channel.send("You already have this role!");
+          return;
+        }
+        if (checkPermissionRole(role)) {
+          message.channel.send("You cannot add yourself to this role.");
+        } else {
+          message.member.roles
+            .add(role)
+            .then(member =>
+              message.channel.send("You were added to this role!")
+            )
+            .catch(err => {
+              console.log(err);
+              message.channel.send("Something went wrong...");
+            });
+        }
+      } else {
+        message.channel.send("Role not found!");
+      }
+    });
   }
 };
