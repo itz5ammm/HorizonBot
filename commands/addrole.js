126837cf75fe6b09bb09333139c0d7368764fa4f
Glitch (tall-held-freon) <none>
2020-08-29 05:00:30 +0000

    🌱🍁 Checkpoint ./commands/addrole.js:20970236/475

--- commands/addrole.js
+++ commands/addrole.js
@@ -8,8 +8,10 @@ module.exports = {
   description: "Aᴅᴅ Rᴏʟᴇ Tᴏ A Usᴇʀ.",
   usage: "<user> <role>",
   execute: async (client, message, args) => {
-    let perm = message.member.hasPermission("MANAGE_ROLES");
-    if (!perm) return message.channel.send("Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Usᴇ Tʜɪs.");
+    if (!message.member.hasPermission("MANAGE_ROLES")) {
+     let embed = new MessageEmbed()
+    .setColor("070707")
+     .setDescription("Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Pᴇʀᴍs Tᴏ Usᴇ Tʜɪs.");
 
     let user = message.mentions.members.first();
     if (!user) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Usᴇʀ.");
@@ -17,8 +19,10 @@ module.exports = {
     let role = message.mentions.roles.first();
     if (!role) return message.channel.send("Mᴇɴᴛɪᴏɴ Tʜᴇ Rᴏʟᴇ.");
 
-  }  else { 
-   
+    else {
+     let role = message.mentions.roles.id();
+      let embed = new MessageEmbed()
+      .setColor("0707("Mᴇɴᴛɪᴏɴ Tʜᴇ Rᴏʟᴇ.");
     
     let embed = new discord.MessageEmbed()
       .setColor("00FFFF")
@@ -27,4 +31,6 @@ module.exports = {
     user.roles.add(role);
     message.channel.send(embed);
   }
-};
+}
+    }
+  }
