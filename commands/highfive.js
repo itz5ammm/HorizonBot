92268f938b86c299d6b866c7233cba3862da1e09
Glitch (tall-held-freon) <none>
2020-08-30 12:21:00 +0000

    🌄✋ Checkpoint ./commands/poke.js:20970236/2002 ./commands/highfive.js:20970236/2067

---
+++ commands/highfive.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const snekfetch = require("snekfetch");
+
+module.exports = {
+  name: "highfive",
+  category: "Action",
+  description: "High-Five someone.",
+  execute: async (client, message, args) => {
+    if (message.mentions.users.size < 1)
+      return message.channel.send("you can't high-five nobody");
+    let user = message.guild.member(message.mentions.users.first());
+    message.channel.send(
+      `${user} You got a high-five from ${message.author.username} ❤`,
+      {
+        embed: {
+          image: {
+            url: "https://i.imgur.com/7BJ6gfM.gif"
+          }
+        }
+      }
+    );
+  }
+};
---
+++ commands/poke.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const snekfetch = require("snekfetch");
+
+module.exports = {
+  name: "poke",
+  category: "Action",
+  description: "Poke A User.",
+  execute: async (client, message, args) => {
+    if (message.mentions.users.size < 1)
+      return message.channel.send("you can't poke nobody");
+    let user = message.guild.member(message.mentions.users.first());
+    message.channel.send(
+      `${user} You got a poke from ${message.author.username} ❤`,
+      {
+        embed: {
+          image: {
+            url: "https://i.imgur.c1om/XMuJ7K8.gif"
+          }
+        }
+      }
+    );
+  }
+};
