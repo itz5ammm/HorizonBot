54f3f5170c0285a7057aa2cc42b70d681b44fdaa
Glitch (tall-held-freon) <none>
2020-08-30 05:31:03 +0000

    🚏💑 Checkpoint ./commands/mute.js:20970236/5525

--- commands/mute.js
+++ commands/mute.js
@@ -1,17 +1,18 @@
 const fs = require("fs");
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
   name: "mute",
   category: "Moderation",
   description: "mute a specific user",
   usage: "[tagged user] [mute time]",
   execute: async (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+    if (!message.member.has.permission("MANAGE_MESSAGES")) {
       let embed = new MessageEmbed()
-      .setColor("070707")
-      .setDescription("You cannot manage messages.");
-  
-    message.channele.send(embed)
+        .setColor("070707")
+        .setDescription("You cannot manage messages.");
+
+      message.channel.send(embed);
     }
 
     let toMute =
@@ -19,27 +20,30 @@ module.exports = {
 
     if (!toMute) {
       let embed = new MessageEmbed()
-      .setColor("070707")
-      .setDescription("You did not specifc a user mention ID!");
-   
-    message.channel.send(embed);
+        .setColor("070707")
+        .setDescription("You did not specifc a user mention ID!");
+
+      message.channel.send(embed);
     }
 
     if (toMute.id == message.author.id) {
       let embed = new MessageEmbed()
-      .setColor("070707")
-      .setDescription("You cannot mute yourself!");
-    
-    message.channel.send(embed);
+        .setColor("070707")
+        .setDescription("You cannot mute yourself!");
+
+      message.channel.send(embed);
     }
 
     if (toMute.highestRole.position >= message.member.highestRole.position) {
-      let embed = new 
-        "You cannot mute a member who is higher or has the same role as you!"
-      );
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription(
+          "You cannot mute a member who is higher or has the same role as you!"
+        );
     }
 
-    let role = message.guild.roles.find(x => x.name == "Muted");
+    let role = message.guild.roles.find(x => x.name === "Muted" || x => x.name ===) "MUTED
+                    ;
 
     if (!role) {
       try {
@@ -61,11 +65,19 @@ module.exports = {
     }
 
     if (toMute.roles.has(role.id)) {
-      return message.channel.send("This user is already muted!");
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription("This user is already muted!");
+
+      message.channel.send(embed);
     }
 
     if (isNaN(args[1])) {
-      return message.reply("Mute time doesn't seem to be a valid number");
+      let embed = new MessageEmbed()
+        .setColor("070707")
+        .setDescription("Mute time doesn't seem to be a valid number");
+
+      message.channel.send(embed);
     }
 
     client.mutes[toMute.id] = {
@@ -73,7 +85,7 @@ module.exports = {
       time: Date.now() + parseInt(args[1]) * 1000
     };
 
-    fs.writeFile("./mute.json", JSON.stringify(client.mutes, null, 4), err => {
+    fs.writeFile("./config.json", JSON.stringify(client.mute, null, 4), err => {
       if (err) {
         throw err;
       }
