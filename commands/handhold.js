86a3045c202b3fd7aa76c865f44de97e38c1c1f4
Glitch (tall-held-freon) <none>
2020-08-30 09:01:03 +0000

    👩🛋 Checkpoint ./commands/handhold.js:20970236/2255 ./commands/urban.js:20970236/4778 ./package.json:20970236/2525

---
+++ commands/handhold.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const snekfetch = require("snekfetch");
+
+module.exports = {
+  name: "handhold",
+  category: "Action",
+  description: "Hold Hands With Anyone.",
+  execute: async (client, message, args) => {
+    if (message.mentions.users.size < 1)
+      return message.channel.send("you can't hold hands with nobody");
+    let user = message.guild.member(message.mentions.users.first());
+    message.channel.send(
+      `${message.author} *Holds* ${user.user.tag}'s Hands, ❤`,
+      {
+        embed: {
+          image: {
+            url: "https://media.giphy.com/media/TnUJHKyjwHXOM/giphy.gif"
+          }
+        }
+      }
+    );
+  }
+};
---
+++ commands/urban.js
@@ -0,0 +1,58 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const snekfetch = require("snekfetch");
+const types = ["top"];
+const urban = require("urban")
+
+module.exports = {
+  name: "urban",
+  category: "Utility",
+  description: "Get The Defination Of A Word.",
+  execute: async (client, message, args) => {
+    const word = args.join(" ");
+    try {
+      const { body } = await snekfetch
+        .get("http://api.urbandictionary.com/v0/define")
+        .query({ term: word });
+      if (!body.list.length)
+        return message.channel.send("Could not find any results.");
+      const data =
+        body.list[
+          types === "top" ? 0 : Math.floor(Math.random() * body.list.length)
+        ];
+      const embed = new Discord.RichEmbed()
+        .setColor(0x32a8f0)
+        .setAuthor(
+          "Urban Dictionary",
+          "https://i.imgur.com/Fo0nRTe.png",
+          "https://www.urbandictionary.com/"
+        )
+        .setURL(data.permalink)
+        .setTitle(data.word)
+        .setDescription(data.definition)
+        .addField("Example", data.example);
+      const filtercheck = [
+        "xxx",
+        "porn",
+        "sex",
+        "18+",
+        "nsfw",
+        "hentai",
+        "dick",
+        "vagina",
+        "pussy"
+      ];
+      if (
+        filtercheck.some(word2 => data.definition.toLowerCase().includes(word2))
+      )
+        return message.channel.send("Not allowed to search nsfw content.");
+      if (filtercheck.some(word3 => data.word.toLowerCase().includes(word3)))
+        return message.channel.send("Not allowed to search nsfw content.");
+      message.channel.send(embed);
+    } catch (err) {
+      return message.channel.send(
+        `Oh no, an error occurred: \`${err.message}\`. Try again later!`
+      );
+    }
+  }
+};
