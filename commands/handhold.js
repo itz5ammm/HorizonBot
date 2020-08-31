af891bcd7bec35f786f2361c59e95fe62fda77d0
Glitch (tall-held-freon) <none>
2020-08-31 08:46:31 +0000

    🏣😽 Checkpoint ./commands/handhold.js:20970236/1567

--- commands/handhold.js
+++ commands/handhold.js
@@ -10,15 +10,15 @@ module.exports = {
     if (message.mentions.users.size < 1)
       return message.channel.send("you can't hold hands with nobody");
     let user = message.guild.member(message.mentions.users.first());
-    const hold = new Discord.RichEmbed()
-      .setColor("070707")
-      .setDescription(`${message.author} *Holds* ${user}'s Hands, ❤`, {
+    message.channel.send(
+      `\`${message.author.tag}\` *Holds* \`${user.user.tag}\`'s Hands, ❤`,
+      {
         embed: {
           image: {
             url: "https://media.giphy.com/media/TnUJHKyjwHXOM/giphy.gif"
           }
         }
-      });
-    message.channel.send(hold);
+      }
+    );
   }
 };
