6fe0d7f24193ff1e6237e94cca443b517b69e62a
Sam376-sch <69468082+Sam376-sch@users.noreply.github.com>
2020-09-03 12:44:47 +0530

    Create decode.js

---
+++ commands/decode.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+  name: "encode",
+  category: "Fun",
+  description: "Encode Stuff.",
+  execute: async (client, message, args) => {
+    if (!args[0]) message.channel.send("What you want me to encode?");
+
+    let Encodemsg = args.slice(0).join(" ");
+
+    let encoded = "";
+    for (var i = 0; i < Encodemsg.length; i++) {
+      let bin = Encodemsg[i].charCodeAt().toString(2);
+      encoded += Array(8 - bin.length + 1).join("0") + bin;
+    }
+
+    let embed = new MessageEmbed()
+      .setTitle("Eɴᴄᴏᴅᴇᴅ")
+      .setColor("RANDOM")
+      .setDescription(encoded);
+
+    message.delete().catch();
+    message.channel.send(embed);
+  }
+};
